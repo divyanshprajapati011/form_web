ba41dca1f4f58a6e3e614465a356c21ede85c51b
Deepak Prajapat <deepakprajapat7067@gmail.com>
2025-05-01 16:01:03 +0530

    Add files via upload

---
+++ run_streamlit.sh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Navigate to your project directory
+cd C:\Users\deepa\OneDrive\Desktop\form_project
+# Run Streamlit app
+streamlit run main.py
+
+chmod +x run_streamlit.sh
+
+./run_streamlit.sh
